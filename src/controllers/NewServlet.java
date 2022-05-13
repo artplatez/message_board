4ca9ccc2e68188c02a39dfbb2348062d59b44f2d
sugyeonglee <sugyeonglee@localhost>
2022-05-13 17:18:36 +0900

    CreateServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -16,25 +16,26 @@ import models.Message;
  */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
     public NewServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // CSRF対策
         request.setAttribute("_token", request.getSession().getId());
 
+        // おまじないとしてのインスタンスを生成
         request.setAttribute("message", new Message());
 
-        RequestDispatcher rd= request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
         rd.forward(request, response);
-	}
+     }
 }
